538b5eeeec5dfa8a71bd563b0bf071aa33ff6020
feliperodri <rms.felipe@gmail.com>
2017-08-09 22:49:40 -0400

    updating version.h
    Signed-off-by: feliperodri <rms.felipe@gmail.com>

--- src/version.h
+++ src/version.h
@@ -1,6 +1,29 @@
+/**
+ * \file version.h
+ *
+ * \brief This header file defines macros that contain version numbers and
+ * 		  strings describing the current version of DSVerifier.
+ *
+ * 		  The version number itself basically consists of three pieces that
+ * 		  are separated by a dot: the major number, the minor number, and
+ * 		  the revision number.
+ *
+ * Authors: Felipe R. Monteiro <rms.felipe@gmail.com>
+ * 			Hussama Ismail <hussamaismail@gmail.com>
+ * 			Iury Bessa <iury.bessa@gmail.com>
+ * 			Lennon C. Chaves <lennon.correach@gmail.com>
+ * 			Lucas C. Cordeiro <lucasccordeiro@gmail.com>
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.md', which is part of this source code package.
+ */
+
 #ifndef DSVERIFIER_VERSION_H
 #define DSVERIFIER_VERSION_H
 
+/** dsverifier_version
+ *  String literal representation of the current software version.
+ */
 #define DSVERIFIER_VERSION "2.0.3"
 
 #endif // DSVERIFIER_VERSION_H
