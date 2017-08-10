36f77b5464128c29dc4948df65e1b4c6445196bb
feliperodri <rms.felipe@gmail.com>
2017-08-09 22:19:09 -0400

    organizing tool boxes
    Signed-off-by: feliperodri <rms.felipe@gmail.com>

---
+++ toolboxes/dsvalidator/toolbox/mode_saturate.m
@@ -0,0 +1,30 @@
+function y = mode_saturate(value, n, l)
+%
+%  Function to saturate mode for arithmetic overflow
+%
+% y = mode_saturate(value, n)
+%
+%  where,
+%  'value' is number to be converted in case of arithmetic
+%  'n' is integer bits implementation
+%  'l' is fractionary bits implementation
+%   the return 'y' is the output converted in saturate mode.
+%
+% Federal University of Amazonas
+% May 15, 2017
+% Manaus, Brazil
+
+y = value;
+
+min_length = -1*(2^(n-1));
+max_length = (2^(n-1)-2^(-1*l));
+
+if value > max_length
+    y = max_length;
+elseif value < min_length
+    y = min_length;
+end
+
+y = fxp_rounding(y,l);
+
+end
---
+++ toolboxes/dsvalidator/toolbox/shiftR.m
@@ -0,0 +1,23 @@
+function [z_out] = shiftR(zIn, z, nZ)
+%
+% Function developed to shift right during the realizations (DFI,DFII and TDFII)
+%
+% Function: [z_out] = shiftR(zIn,z, nZ)
+%
+%
+% The main objective of this function is flip the vector in the left-right
+% direction and including a value in the beginning of vector in each interaction of the realization.
+%
+% Federal University of Amazonas
+% May 15, 2017
+% Manaus, Brazil
+
+for i=nZ:-1:2
+    z(i) = z(i-1);
+end
+
+z(1) = zIn;
+
+z_out = z;
+
+end
