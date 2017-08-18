b70f49fe21f62fa7cd938638579fe008f581ef42
Felipe R. Monteiro <rms.felipe@gmail.com>
2017-08-18 19:19:53 -0400

    uploading Utils
    Signed-off-by: Felipe R. Monteiro <rms.felipe@gmail.com>

---
+++ src/Utils.h
@@ -0,0 +1,20 @@
+/**
+ * \file Utils.h
+ *
+ * \brief //TODO
+ *
+ * Authors: Felipe R. Monteiro <rms.felipe@gmail.com>
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.md', which is part of this source code package.
+ */
+
+#ifndef SRC_UTILS_H_
+#define SRC_UTILS_H_
+
+namespace utils
+{
+
+}
+
+#endif /* SRC_UTILS_H_ */
