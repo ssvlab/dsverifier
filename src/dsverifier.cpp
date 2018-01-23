acbda4f3d6da76bcf8e9e6ac05d6a0a5b6558473
thiago-cavalcante <thiagorodrigoengcomp@gmail.com>
2018-01-23 09:20:00 -0400

    inserting fwl effects test

--- src/dsverifier.cpp
+++ src/dsverifier.cpp
@@ -3276,13 +3276,17 @@ void closed_loop()
 {
   fxp_t K_fxp[LIMIT][LIMIT];
   double result1[LIMIT][LIMIT];
-  double test2, test=8.675;
+  double test2, test3, test4, test=8.675;
   test2 = fxp_double_to_fxp(test);
+  test3 = fxp_to_double(test2);
+  test4 = fxp_to_float(test2);
   std::cout << "before:" << test << std::endl;
   std::cout << "before:" << (float)test << std::endl;
   std::cout << "before2:" << test2 << std::endl;
   std::cout << "after:" << (double)test2 << std::endl;
   std::cout << "after2:" << (float)test2 << std::endl;
+  std::cout << "before3:" << test3 << std::endl;
+  std::cout << "before4:" << test4 << std::endl;
 
   int i, j, k;
   for(i = 0; i < LIMIT; i++)
