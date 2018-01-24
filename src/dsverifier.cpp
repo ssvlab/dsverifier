c5846715e29214f081b14cf822574134e782e7b0
thiago-cavalcante <thiagorodrigoengcomp@gmail.com>
2018-01-24 14:08:31 -0400

    getting experiments ready

--- src/dsverifier.cpp
+++ src/dsverifier.cpp
@@ -3286,14 +3286,14 @@ void closed_loop()
   for(i = 0; i < LIMIT; i++)
     for(j = 0; j < LIMIT; j++)
       result1[i][j] = 0;
-  //if(nofwl!=true)
-  //{
+  if(nofwl!=true)
+  {
     for(i = 0; i < _controller.nStates; i++)
     {
       K_fxp[0][i] = fxp_double_to_fxp(_controller.K[0][i]);
       _controller.K[0][i] = fxp_to_double(K_fxp[0][i]);
     }
-  //}
+  }
   // B*K
   double_matrix_multiplication(_controller.nStates, _controller.nInputs,
       _controller.nInputs, _controller.nStates, _controller.B, _controller.K,
